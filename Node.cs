4485bd1e70283ddad03c157e948ccf11f2579cd9
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2019-12-26 17:15:30 +0300

    Add Node.cs

---
+++ Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Mining_Decision_Tree_Using_CART
+{
+    class Node
+    {
+        private string explanation;
+        private List<List<Data>> datas;
+
+        public Node left ,right;
+
+         
+
+
+    }
+}
