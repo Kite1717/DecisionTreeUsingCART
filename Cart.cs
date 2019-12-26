b0237ca1e4bc27d5fae67a011608ce100dbd7aca
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2019-12-26 17:15:02 +0300

    Add cart class

---
+++ Cart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Mining_Decision_Tree_Using_CART
+{
+    class Cart
+    {
+        private List<List<Data>> fullData;
+        private List<List<Data>> uniqueData;
+        private List<int> indexOfContinuousData;
+
+        public Cart(List<List<Data>> fullData , List<List<Data>> uniqueData,List<int> indexOfContinuousData)
+        {
+            this.fullData = fullData;
+            this.uniqueData = uniqueData;
+            this.indexOfContinuousData = indexOfContinuousData;
+        }
+
+
+        public void applyCART()
+        {
+
+
+
+        }
+
+        private double calculateTethaValue()
+        {
+
+
+        }
+    }
+}
