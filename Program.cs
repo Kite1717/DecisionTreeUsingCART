d5f2cfb4a1b67c7a190f5398da2a4027937097a6
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2019-12-26 17:14:43 +0300

    Add base class

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Mining_Decision_Tree_Using_CART
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataEditor.GetData();
+            DataEditor.MakeDataUnique();
+
+            Cart cart = new Cart()
+            Console.ReadKey();
+
+        }
+    }
+}
